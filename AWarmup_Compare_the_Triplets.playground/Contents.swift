015de93169c43004dd3890f54ab29b63b463f4ef
Carlos Filipe R. de Oliveira <cfsundog@gmail.com>
2017-01-30 18:38:36 -0200

    solution for AWarmup_Compare_the_Triplets

--- AWarmup_Compare_the_Triplets.playground/Contents.swift
+++ AWarmup_Compare_the_Triplets.playground/Contents.swift
@@ -1,5 +1,29 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+let A = [1,2,3]
+let B = [1,2,3]
+
+//// read array and map the elements to integer
+//let A = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+//let B = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+var Alice = 0
+var Bob = 0
+
+for var i in 0..<A.count {
+    guard A[i] != B[i] else {
+        continue
+    }
+    
+    if A[i] < B[i] {
+        Bob += 1
+    } else {
+        Alice += 1
+    }
+    
+}
+
+
+print(Alice, Bob)
